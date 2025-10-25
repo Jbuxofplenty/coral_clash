a78fd638fe1add61b83b74cc1fa83c1ba477fd06
Josiah Buxton <jbuxofplenty@gmail.com>
2025-10-25 08:55:21 -0400

    feat: CI Fix

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -1,14 +1,13 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/__tests__/setup.js'],
+    testPathIgnorePatterns: ['/node_modules/'],
     collectCoverageFrom: [
         'routes/**/*.js',
         'utils/**/*.js',
         '!utils/notifications.js', // Skip notifications for now
     ],
     coveragePathIgnorePatterns: ['/node_modules/'],
-    setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
     // Transform shared TypeScript files
     transformIgnorePatterns: ['node_modules/(?!(firebase-admin|firebase-functions|@firebase)/)'],
     moduleNameMapper: {
