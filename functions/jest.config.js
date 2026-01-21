533db47d0db6d5bc49e6477caac2a2362c1af832
Adam Winchell <winchell2326adam@gmail.com>
2026-01-21 16:19:30 -0500

    test(server): fix EISDIR error in GitHub Actions by adding package name to moduleNameMapper

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -16,6 +16,7 @@ export default {
         '/node_modules/(?!(firebase-functions|firebase-admin|@jbuxofplenty/coral-clash)/)'
     ],
     moduleNameMapper: {
+        '^@jbuxofplenty/coral-clash$': '<rootDir>/__mocks__/shared-game.js',
         '^../shared/dist/game$': '<rootDir>/__mocks__/shared-game.js',
         '^../shared/dist/game/index.js$': '<rootDir>/__mocks__/shared-game.js',
     },
